2710808773ec2e14b23d2f9b30ec79cfed0eb3ab
Vishnu-Aithal <90306959+Vishnu-Aithal@users.noreply.github.com>
2022-01-21 02:53:10 +0530

    feat-add drawer component

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+//Chips Functionality
+
 const addNewChip = function (text) {
   let chipsWrapper = document.querySelector(".chips-wrapper");
   let newChip = document.createElement("div");
@@ -23,3 +25,37 @@ newChipInput.addEventListener("keyup", (event) => {
 });
 
 window.onload = () => addNewChip("Chip 1");
+
+//Drawer Functionality
+
+const drawerListItems = document.querySelectorAll(".drawer-list-item");
+let activeItem;
+drawerListItems.forEach(
+  (item) =>
+    (item.onclick = () => {
+      if (activeItem) activeItem.classList.remove("selected");
+      item.classList.add("selected");
+      activeItem = item;
+    })
+);
+
+let mainDrawerCollapse = document.querySelector(".main-drawer-collapse");
+mainDrawerCollapse.onclick = () => {
+  let mainList = document.querySelector(".drawer-list-main");
+  mainList.classList.toggle("collapsed");
+  mainList.classList.contains("collapsed")
+    ? (mainDrawerCollapse.innerHTML = `<i class="fas fa-angle-down fa-2x"></i>`)
+    : (mainDrawerCollapse.innerHTML = `<i class="fas fa-angle-up fa-2x"></i>`);
+};
+
+document.querySelectorAll(".drawer-head-sub").forEach(
+  (subList) =>
+    (subList.onclick = (e) => {
+      let currentSubDrawerList = e.target.parentElement.children[1];
+      let currentArrowIcon = e.target.children[2];
+      currentSubDrawerList.classList.toggle("collapsed");
+      currentSubDrawerList.classList.contains("collapsed")
+        ? (currentArrowIcon.innerHTML = `<i class="fas fa-angle-right"></i>`)
+        : (currentArrowIcon.innerHTML = `<i class="fas fa-angle-up"></i>`);
+    })
+);
